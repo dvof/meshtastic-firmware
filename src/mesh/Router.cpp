197a96cf3ec56ff1e273c242250f0ee78e5693da
Daan v O <daan.vo@hotmail.com>
2025-10-06 19:27:04 +0200

    Working with Stary's temp fix, revert static memory pool allocation

--- src/mesh/Router.cpp
+++ src/mesh/Router.cpp
@@ -43,7 +43,8 @@ Allocator<meshtastic_MeshPacket> &packetPool = dynamicPool;
     (MAX_RX_TOPHONE + MAX_RX_FROMRADIO + 2 * MAX_TX_QUEUE +                                                                      \
      2) // max number of packets which can be in flight (either queued from reception or queued for sending)
 
-static MemoryPool<meshtastic_MeshPacket, MAX_PACKETS_STATIC> staticPool;
+//static MemoryPool<meshtastic_MeshPacket, MAX_PACKETS_STATIC> staticPool;
+static MemoryDynamic<meshtastic_MeshPacket> staticPool;
 Allocator<meshtastic_MeshPacket> &packetPool = staticPool;
 #endif
 
